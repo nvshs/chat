0c3242556964ce4a3c9b8025b8bd9f24af6b7f92
What-Question-Mark <97435840+What-Question-Mark@users.noreply.github.com>
2022-10-30 18:33:08 +1000

    a

--- utils/index.js
+++ utils/index.js
@@ -1,46 +1,81 @@
 function usernameChange() {
-    const usernameInput = document.getElementById('usernameInput').value;
-    if (usernameInput.length > 2){
-        document.getElementById('usernameInput').value = username;
-    }
-    if (usernameInput.includes("<") || usernameInput.includes(">")){
-        document.getElementById('usernameInput').value = "New User";
-    }
-    else {
+    const username = document.getElementById('usernameInput').value;
+
+    if (username.length < 2){
         document.getElementById('usernameInput').value = "";
+    }
+    if (username.includes("<") || username.includes(">")){
+        document.getElementById('usernameInput').value = "New User";
     }
 }
 
 function outputMessages(username, text, time) {
     const messageContainer = document.querySelector('.chat-messages');
     const messageBox = document.getElementById('msg');
-    var html = '';
-    html += '<div class="message">';
-    html += '<p class="meta">' + username + ' <span style="color: var(--dark-color-d)">' + time +'</span></p>';
-    html += '<p class="text">' + text +'</p>';
-    html += '</div>';
 
-    if (username.length == 0){
-        username = "New User"
-        document.getElementById('usernameInput').value = "New User";
-    }
-    if (text.includes("<") || text.includes(">")){
-        html = '';
-        html += `<div class="message">`;
-        html += `<p style="color: var(--theme-color)" class="meta">Bot <span style="color: var(--dark-color-d)">${time}</span></p>`;
-        html += `<p class="text"><b>@${username}</b>, that message contains words or phrases that are banned!</p>`;
-        html += `</div>`;
-    }
-    if (text.length > 0){
-        messageContainer.insertAdjacentHTML('beforeend', html);
-        messageContainer.scrollTop = messageContainer.scrollHeight;
+    if (username.includes("adminaccess")){
+        var username = username.replace("adminaccess","");
+        
+        if (text.startsWith('!')) {
+            //if (text == '!'){
+            //    if (text == '!test '){
+            //        var html = '';
+            //          html += '<div class="message">';
+            //          html += `<p style="color: var(--error-color)" class="meta">${username} <span style="color: var(--dark-color-d)">${time}</span></p>`;
+            //          html += '<p class="text">' + test1[1].join(" ") +'</p>';
+            //          html += '</div>';
+            //  
+            //        messageContainer.insertAdjacentHTML('beforeend', html);
+            //        messageContainer.scrollTop = messageContainer.scrollHeight;
+            //    
+            //        document.querySelector('.message:last-child').scrollIntoView({
+            //            behavior: 'smooth'
+            //        });
+            //        messageBox.value = '';
+            //    }
+            //}
+        } else {
+            if (text.length > 0){
+                var html = '';
+                html += '<div class="message">';
+                html += `<p style="color: var(--error-color)" class="meta">${username} <span style="color: var(--dark-color-d)">${time}</span></p>`;
+                html += '<p class="text">' + text +'</p>';
+                html += '</div>';
+        
+                messageContainer.insertAdjacentHTML('beforeend', html);
+                messageContainer.scrollTop = messageContainer.scrollHeight;
+            
+                document.querySelector('.message:last-child').scrollIntoView({
+                    behavior: 'smooth'
+                });
+                messageBox.value = '';
+            } else {
+                messageBox.value = '';        
+            }
+        } 
+    } else {
+        if (text.includes("<") || text.includes(">")){
+            html = '';
+            html += `<div class="message">`;
+            html += `<p style="color: var(--theme-color)" class="meta">Bot <span style="color: var(--dark-color-d)">${time}</span></p>`;
+            html += `<p class="text"><b>@${username}</b>, that message contains words or phrases that are banned!</p>`;
+            html += `</div>`;
+        } if (text.length > 0){
+            var html = '';
+            html += '<div class="message">';
+            html += '<p class="meta">' + username + ' <span style="color: var(--dark-color-d)">' + time +'</span></p>';
+            html += '<p class="text">' + text +'</p>';
+            html += '</div>';
     
-        document.querySelector('.message:last-child').scrollIntoView({
-            behavior: 'smooth'
-        });
-        messageBox.value = '';
-    }
-    else {
-        messageBox.value = '';        
+            messageContainer.insertAdjacentHTML('beforeend', html);
+            messageContainer.scrollTop = messageContainer.scrollHeight;
+        
+            document.querySelector('.message:last-child').scrollIntoView({
+                behavior: 'smooth'
+            });
+            messageBox.value = '';
+        } else {
+            messageBox.value = '';        
+        }
     }
 }
